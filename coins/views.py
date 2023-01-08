8ed768f56395b7c1a6a186d541b94e34be11a2b2
Folkor1 <igorvasiljevs@gmail.com>
2023-01-08 18:25:52 +0000

    Add filters for coins

--- coins/views.py
+++ coins/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Metal, Coins
 
 
@@ -7,16 +9,51 @@ def all_coins(request):
     A view to show all coins
     """
     coins = Coins.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('coins'))
+
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query) |
+                Q(origin__icontains=query) |
+                Q(condition__icontains=query) |
+                Q(year__icontains=query)
+                )
+            coins = coins.filter(queries)
+
+    if 'era' in request.GET:
+        eras = request.GET['era'].split(',')
+        coins = coins.filter(era__in=eras)
+        eras = Coins.objects.filter(name__in=eras)
+
+    if 'condition' in request.GET:
+        conditions = request.GET['condition'].split(',')
+        coins = coins.filter(condition__in=conditions)
+        conditions = Coins.objects.filter(name__in=conditions)
+
+    if 'metal' in request.GET:
+        metals = request.GET['metal'].split(',')
+        coins = coins.filter(metal__name__in=metals)
+        metals = Metal.objects.filter(name__in=metals)
 
     context = {
         'coins': coins,
+        'search_term': query,
     }
 
     return render(request, 'coins/coins.html', context)
 
 
 def coins_detail(request, coins_id):
-    """ A view to show individual coins details """
+    """
+    A view to show individual coin details
+    """
 
     coins = get_object_or_404(Coins, pk=coins_id)
 
