89bea08510462cc52c1642399eb8388bd8759d8b
Folkor1 <igorvasiljevs@gmail.com>
2023-01-13 21:55:03 +0000

    Add checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.conf import settings
 from .forms import OrderForm
-from .models import Order, OrderLineItem
-from products.models import Coins
-from profiles.models import UserProfile
-from profiles.forms import UserProfileForm
-from bag.contexts import cart_contents
 
-import stripe
-import json
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('coins'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
